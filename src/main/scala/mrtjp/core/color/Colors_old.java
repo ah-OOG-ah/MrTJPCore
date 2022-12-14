1078ab3192b0cf430cb55079dfee44d29b477f49
glowredman <35727266+glowredman@users.noreply.github.com>
2022-12-14 23:47:13 +0100

    Apply spotless

--- src/main/scala/mrtjp/core/color/Colors_old.java
+++ src/main/scala/mrtjp/core/color/Colors_old.java
@@ -14,8 +14,7 @@ import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
 
 @Deprecated
-public enum Colors_old
-{
+public enum Colors_old {
     WHITE(0xFFFFFF),
     ORANGE(0xC06300),
     MAGENTA(0xB51AB5),
@@ -34,29 +33,42 @@ public enum Colors_old
     BLACK(0x1F1F1F);
 
     public static final Colors_old[] VALID_COLORS = values();
-    private static final String[] dyeDictionary = { "dyeBlack", "dyeRed", "dyeGreen", "dyeBrown", "dyeBlue", "dyePurple", "dyeCyan", "dyeLightGray", "dyeGray", "dyePink", "dyeLime", "dyeYellow", "dyeLightBlue", "dyeMagenta", "dyeOrange", "dyeWhite" };
+    private static final String[] dyeDictionary = {
+        "dyeBlack",
+        "dyeRed",
+        "dyeGreen",
+        "dyeBrown",
+        "dyeBlue",
+        "dyePurple",
+        "dyeCyan",
+        "dyeLightGray",
+        "dyeGray",
+        "dyePink",
+        "dyeLime",
+        "dyeYellow",
+        "dyeLightBlue",
+        "dyeMagenta",
+        "dyeOrange",
+        "dyeWhite"
+    };
 
-    private Colors_old(int rgb)
-    {
+    private Colors_old(int rgb) {
         name = name().substring(0, 1) + name().substring(1).toLowerCase();
         this.rgb = rgb;
-        rgba = rgb<<8|0xFF;
-        argb = 0xFF000000|rgb;
-        c = new ColourRGBA(rgb<<8|0xFF);
+        rgba = rgb << 8 | 0xFF;
+        argb = 0xFF000000 | rgb;
+        c = new ColourRGBA(rgb << 8 | 0xFF);
     }
 
-    public int dyeId()
-    {
+    public int dyeId() {
         return 15 - ordinal();
     }
 
-    public int woolId()
-    {
+    public int woolId() {
         return ordinal();
     }
 
-    public String getOreDict()
-    {
+    public String getOreDict() {
         return dyeDictionary[dyeId()];
     }
 
@@ -66,30 +78,33 @@ public enum Colors_old
     public final int rgba;
     public final int argb;
 
-    public static Colors_old get(int i)
-    {
-        if (i > 15)
-            return BLACK;
-        if (i < 0)
-            return WHITE;
+    public static Colors_old get(int i) {
+        if (i > 15) return BLACK;
+        if (i < 0) return WHITE;
         return VALID_COLORS[i];
     }
 
-    public ItemStack getDye()
-    {
+    public ItemStack getDye() {
         return new ItemStack(Items.dye, 1, dyeId());
     }
 
-    public float rF() { return (rgb>>16&255)/255.0F; }
-    public float gF() { return (rgb>>8&255)/255.0F; }
-    public float bF() { return (rgb&255)/255.0F; }
+    public float rF() {
+        return (rgb >> 16 & 255) / 255.0F;
+    }
+
+    public float gF() {
+        return (rgb >> 8 & 255) / 255.0F;
+    }
+
+    public float bF() {
+        return (rgb & 255) / 255.0F;
+    }
 
     @SideOnly(Side.CLIENT)
-    public void setGL11Color(float alpha)
-    {
-        float r=(rgb>>16&255)/255.0F;
-        float g=(rgb>>8&255)/255.0F;
-        float b=(rgb&255)/255.0F;
+    public void setGL11Color(float alpha) {
+        float r = (rgb >> 16 & 255) / 255.0F;
+        float g = (rgb >> 8 & 255) / 255.0F;
+        float b = (rgb & 255) / 255.0F;
         GL11.glColor4f(r, g, b, alpha);
     }
 }
--- src/main/scala/mrtjp/core/util/PostponedWorkItem.java
+++ src/main/scala/mrtjp/core/util/PostponedWorkItem.java
@@ -11,38 +11,31 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author Andrey Redko - http://aredko.blogspot.com
  */
-public class PostponedWorkItem<T> implements Delayed
-{
+public class PostponedWorkItem<T> implements Delayed {
     private final long origin;
     private final long delay;
     private final T workItem;
 
-    public PostponedWorkItem(final T workItem, final long delay)
-    {
+    public PostponedWorkItem(final T workItem, final long delay) {
         this.origin = System.currentTimeMillis();
         this.workItem = workItem;
         this.delay = delay;
     }
 
-    public T getItem()
-    {
+    public T getItem() {
         return workItem;
     }
 
     @Override
-    public long getDelay(TimeUnit unit)
-    {
+    public long getDelay(TimeUnit unit) {
         return unit.convert(delay - (System.currentTimeMillis() - origin), TimeUnit.MILLISECONDS);
     }
 
     @Override
-    public int compareTo(Delayed delayed)
-    {
-        if (delayed == this)
-            return 0;
+    public int compareTo(Delayed delayed) {
+        if (delayed == this) return 0;
 
-        if (delayed instanceof PostponedWorkItem)
-        {
+        if (delayed instanceof PostponedWorkItem) {
             long diff = delay - ((PostponedWorkItem) delayed).delay;
             return ((diff == 0) ? 0 : ((diff < 0) ? -1 : 1));
         }
